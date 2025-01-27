a8c1998c28260f13eb7b6e5b99885a2556e3b748
muslima <muslima@gmail.com>
2025-01-27 10:27:37 +0500

    tugadi

---
+++ app.js
@@ -0,0 +1,55 @@
+// 1- misol
+// const name=["M" ,"U" ,"S" ,"L" ,"I" ,"M" ,"A"]
+// console.log(name.at())
+
+// 2- misol
+// const numbers = [4, 5, 6, 7, 8, 9, 10];
+// const newNumbers = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   newNumbers.push(numbers[i] ** 2);
+// }
+
+// console.log(newNumbers);
+
+// 3- misol
+// const numbers = [1, 4, 5, 4, 6, 7, 8, 9, 7, 10, 6];
+// const newNumbers = [];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (!newNumbers.includes(numbers[i])) {
+//     newNumbers.push(numbers[i]);
+//   }
+// }
+
+// console.log(newNumbers);
+
+// 4- misol
+// let ism = "Umidaxon".split("").reverse().join("");
+// console.log(ism);
+
+// 5- misol
+// let ism = "Umidaxon".split("");
+// let reversedName = [];
+// for (let i = 0; i < ism.length; i++) {
+//   reversedName.unshift(ism[i]);
+// }
+// console.log(reversedName.join(""));
+// let text =
+//   "   Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, dignissimosamet numquam blanditiis impedit, distinctio corrupti aliquam illo saepe abid minima dolore ipsam deleniti. Porro eveniet neque quisquam nostrum!".split(
+//     ""
+//   );
+// let counter = 0;
+// for (let i = 0; i < text.length; i++) {
+//   if ("a" == text[i]) {
+//     counter++;
+//   }
+// }
+// console.log(counter);
+
+// 6- misol
+// const numbers = [];
+// for (let i = 0; i < 10; i++) {
+//   numbers.push(Math.floor(Math.random() * 100) + 1);
+// }
+// console.log(numbers);
+
